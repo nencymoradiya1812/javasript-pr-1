49c581f0c65ed11342ee727803fe20c412e35c21
nencymoradiya1812 <nencymoradiya18@gmail.com>
2023-06-06 11:07:56 +0530

    login page and signin page completed

--- javascript/login.js
+++ javascript/login.js
@@ -1,3 +1,18 @@
 import nav from "../components/Nav.js";
 
-document.getElementById("nav").innerHTML=nav()
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  let user=JSON.parse(localStorage.getItem("user"));
+  console.log(user);
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  if(email==user.email && password==user.password){
+    alert("logged in");
+    localStorage.setItem("loggedIn",true);
+    window.location.href="/javasript-pr-1/index.html";
+  }else{
+    alert("Invalid");
+  }
+});
+console.log(nav)
+document.getElementById("nav").innerHTML = nav();
--- javascript/signin.js
+++ javascript/signin.js
@@ -1,3 +1,15 @@
 import nav from "../components/Nav.js";
 
-document.getElementById("nav").innerHTML=nav()
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  let user = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+    conpassword: document.getElementById("conpassword").value,
+    number: document.getElementById("number").value,
+  };
+  localStorage.setItem("user",JSON.stringify(user));
+  console.log(user);
+});
+document.getElementById("nav").innerHTML = nav();
